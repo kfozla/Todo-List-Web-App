ca09d40f859ce796b3a57f835aa83ee04da4fd21
Yusuf Gökdağ <yusuf@yusuf-MBP.home>
2024-06-23 20:43:26 +0300

    added hello world controller

--- src/main/java/com/example/springboot/mywebapp/hello/SayHelloController.java
+++ src/main/java/com/example/springboot/mywebapp/hello/SayHelloController.java
@@ -1,5 +1,15 @@
 package com.example.springboot.mywebapp.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SayHelloController {
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello! What are you doing?";
+    }
 
 }
