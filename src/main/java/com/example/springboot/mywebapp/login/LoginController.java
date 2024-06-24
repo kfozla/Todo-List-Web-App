9bfbd8eec5e695d044bcb6efb0b35f8ba4a14a7c
Yusuf Gökdağ <yusufgokdag07@gmail.com>
2024-06-24 16:52:56 +0300

    request param added

--- src/main/java/com/example/springboot/mywebapp/login/LoginController.java
+++ src/main/java/com/example/springboot/mywebapp/login/LoginController.java
@@ -1,12 +1,15 @@
 package com.example.springboot.mywebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     @RequestMapping("/login")
-    public String  loginPage(){
+    public String  loginPage(@RequestParam String name, ModelMap model){
+        model.put("name",name);
         return "login";
     }
 }
