a4a1b24dc8060ab53c4f62c14630726102798f0b
Yusuf Gökdağ <yusufgokdag07@gmail.com>
2024-06-24 20:01:19 +0300

    cleared up

--- src/main/java/com/example/springboot/mywebapp/login/LoginController.java
+++ src/main/java/com/example/springboot/mywebapp/login/LoginController.java
@@ -1,15 +1,16 @@
 package com.example.springboot.mywebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
     @RequestMapping("/login")
-    public String  loginPage(@RequestParam String name, ModelMap model){
-        model.put("name",name);
+    public String  loginPage(){
+
         return "login";
     }
 }
