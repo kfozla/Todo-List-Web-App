4ca66438c3356173b10af2fb1d52a099fd96c3af
Yusuf Gökdağ <yusufgokdag07@gmail.com>
2024-06-24 20:18:51 +0300

    added welcome jsp and form created for user input

--- src/main/java/com/example/springboot/mywebapp/login/LoginController.java
+++ src/main/java/com/example/springboot/mywebapp/login/LoginController.java
@@ -3,14 +3,24 @@ package com.example.springboot.mywebapp.login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     private Logger logger= LoggerFactory.getLogger(this.getClass());
-    @RequestMapping("/login")
-    public String  loginPage(){
+    @RequestMapping(value ="/login",method = RequestMethod.GET)
+    public String  loginPageGet(){
 
         return "login";
+    }
+    @RequestMapping(value ="/login",method = RequestMethod.POST)
+    public String  loginPagePost(@RequestParam String name, @RequestParam String password, ModelMap modelmap){
+        modelmap.put("name",name);
+        modelmap.put("password",password);
+
+        return "welcome";
     }
 }
